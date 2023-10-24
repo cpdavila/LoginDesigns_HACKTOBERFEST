128e90d28a53e5b2f411435a109543a632062240
Derian Pinto <pintoderian@hotmail.com>
2023-10-24 16:53:45 -0500

    Config server with vite, tailwind, and react

---
+++ src/main.jsx
@@ -0,0 +1,25 @@
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  BrowserRouter as Router,
+  useRoutes,
+} from 'react-router-dom'
+
+import routes from '~react-pages'
+import './index.css'
+
+const App = () => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      {useRoutes(routes)}
+    </Suspense>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>,
+)
